d8c933513e4323e90047c3ccc6639b411064eef3
yue.lab <gitlab@rainy.me>
2018-11-25 21:08:55 +0900

    move to pixi

---
+++ precache-manifest.78958240516b3d22eaa45b8ea6b29ff6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "96c51cd37770635a13c4",
+    "url": "/static/js/main.96c51cd3.chunk.js"
+  },
+  {
+    "revision": "d717b955a3e288bef1a0",
+    "url": "/static/js/1.d717b955.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff54eb4bbb098014b187d431ae92d02e",
+    "url": "/index.html"
+  }
+];
